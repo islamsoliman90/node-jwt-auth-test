29ec352fc02aa956f7f131a7d4c83eadeb527eb8
islamsoliman90 <islam.soliman1990@gmail.com>
2023-09-15 16:47:57 +0300

    handle errors

--- controlers/user.js
+++ controlers/user.js
@@ -1,5 +1,18 @@
 const User = require("../module/user");
 
+const handle_erros = (err) => {
+  console.log(err.message, err.code);
+  const errors = { email: "", pass: "" };
+  if (err.code === 11000) {
+    errors.email = "the email is existed";
+  }
+  if (err.message.includes("validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
 module.exports.Get_login = (req, res) => {
   res.render("login");
 };
@@ -12,8 +25,8 @@ module.exports.post_signUp = async (req, res) => {
     const user = await User.create({ email, pass });
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(400).send("error,user not created");
+    let errors = handle_erros(err);
+    res.status(400).json(errors);
   }
 };
 
--- module/user.js
+++ module/user.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Email Is Required"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Faild Email Validation"],
   },
   pass: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, "password is required"],
+    minlength: [6, "Password Must be Atleast 6 Letter "],
   },
 });
 
